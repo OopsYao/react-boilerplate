9574d2acb5fc909fb8218b68f7a83a30a8aa3893
oopsy <whoops.yao@gmail.com>
2020-08-17 12:29:03 +0800

    ✨ Add HtmlWebpackPlugin and WebDevServer support

---
+++ src/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'normalize.css'
+import './style.css'
+import styles from './.module.css'
+
+const App = () => {
+  return (
+    <div className={styles.root}>
+      <code>Hello World</code>
+    </div>
+  )
+}
+
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,39 @@
-module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|mjs)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader'],
-      },
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = (env, options) => {
+  const onProduction = options.mode === 'production'
+  return {
+    entry: './src/index.js',
+    mode: 'development',
+    devtool: !onProduction && 'eval-source-map',
+    devServer: {
+      clientLogLevel: 'silent',
+      open: true,
+      hot: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|mjs)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader', 'eslint-loader'],
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            { loader: 'css-loader', options: { importLoaders: 1 } },
+            'postcss-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        hash: true,
+        title: 'React Boilerplate',
+        template: './src/index.html',
+      }),
     ],
-  },
+  }
 }
